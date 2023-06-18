251f9277faf9ff7c51fa4ba4f79c13353dbc7993
wirelessEye <okuloj@outlook.com>
2023-06-18 13:40:36 +1000

    Update itemRouter

--- src/server/api/routes/items.ts
+++ src/server/api/routes/items.ts
@@ -1,44 +1,44 @@
-import { publicProcedure, router } from "../trpc";
+import { protectedProcedure, router } from "../trpc";
 import { z } from "zod";
-import { v4 as uuidv4 } from "uuid";
+import ItemController from "../../controllers/item-controller";
 
 export const itemRouter = router({
-    getItem: publicProcedure
+    getItem: protectedProcedure
         .input(z.object({ itemId: z.string() }))
-        .query(async ({ ctx, input }) => {
-            const item = await ctx.prisma.item.findFirstOrThrow({
-                where: {
-                    id: input.itemId,
-                },
-            });
-
+        .query(async ({ input }) => {
+            const item = await ItemController.getItem(input.itemId);
             return item;
         }),
-    getItems: publicProcedure.query(async ({ ctx }) => {
-        const items = await ctx.prisma.item.findMany();
+    getItems: protectedProcedure.query(async ({ ctx }) => {
+        const items = await ItemController.getItems(ctx.userId);
         return items;
     }),
-    createItem: publicProcedure
-        .input(z.object({ title: z.string() }))
+    createItem: protectedProcedure
+        .input(
+            z.object({
+                title: z.string(),
+                url: z.string(),
+                description: z.string(),
+                collectionName: z.string(),
+                tagNames: z.string().array(),
+                thumbnail: z.string().nullable(),
+            })
+        )
         .mutation(async ({ ctx, input }) => {
-            const item = await ctx.prisma.item.create({
-                data: {
-                    id: uuidv4(),
-                    title: input.title,
-                },
-            });
-
+            const item = await ItemController.createItem(
+                input.title,
+                input.url,
+                input.description,
+                input.collectionName,
+                input.tagNames,
+                input.thumbnail,
+                ctx.userId
+            );
             return item;
         }),
-    deleteItem: publicProcedure
+    deleteItem: protectedProcedure
         .input(z.object({ id: z.string() }))
         .mutation(async ({ ctx, input }) => {
-            const item = await ctx.prisma.item.delete({
-                where: {
-                    id: input.id,
-                },
-            });
-
-            return item;
+            ItemController.deleteItem(input.id, ctx.userId);
         }),
 });
