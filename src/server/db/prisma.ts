6fa9a196912b94588d0234bf763e096b7123f3fd
wirelessEye <okuloj@outlook.com>
2023-06-21 18:46:41 +1000

    Solve prisma many clients issue

--- src/server/db/prisma.ts
+++ src/server/db/prisma.ts
@@ -1,3 +1,19 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+class DBClient {
+    private static instance: DBClient;
+    public prisma: PrismaClient;
+
+    private constructor() {
+        this.prisma = new PrismaClient();
+    }
+
+    public static getInstance() {
+        if (!DBClient.instance) {
+            DBClient.instance = new DBClient();
+        }
+        return DBClient.instance;
+    }
+}
+
+export const prisma = DBClient.getInstance().prisma;
