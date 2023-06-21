b118d373edaa1d506add257b6709bfa636c828e2
wirelessEye <okuloj@outlook.com>
2023-06-21 22:19:00 +1000

    Revert changes to prisma.ts

--- src/server/db/prisma.ts
+++ src/server/db/prisma.ts
@@ -1,19 +1,3 @@
 import { PrismaClient } from "@prisma/client";
 
-class DBClient {
-    private static instance: DBClient;
-    public prisma: PrismaClient;
-
-    private constructor() {
-        this.prisma = new PrismaClient();
-    }
-
-    public static getInstance() {
-        if (!DBClient.instance) {
-            DBClient.instance = new DBClient();
-        }
-        return DBClient.instance;
-    }
-}
-
-export const prisma = DBClient.getInstance().prisma;
+export const prisma = new PrismaClient();
