b4b458dba23feaa1596bcb16abde457edf8ade13
Gordon Huang <dqna64@gmail.com>
2023-06-24 01:38:43 +1000

    add collection and tags selectors to item cards

---
+++ src/app/components/ItemCard.tsx
@@ -0,0 +1,87 @@
+import { LuPackage2, LuTag } from "react-icons/lu";
+import {
+    Card,
+    CardHeader,
+    CardTitle,
+    CardDescription,
+    CardContent,
+} from "./Card";
+import { CollectionSelector } from "./CollectionSelector";
+import { TagSelector } from "./TagSelector";
+import { useState } from "react";
+import { trpc } from "../utils/trpc";
+import { removeTag, setTag } from "../utils";
+import { Item, Collection, Tag } from "@prisma/client";
+
+export default function ItemCard({
+    data,
+}: {
+    data: Item & { collection: Collection; tags: Tag[] };
+}) {
+    const collectionsQuery = trpc.collection.getCollections.useQuery();
+    const tagsQuery = trpc.tag.getTags.useQuery();
+
+    const [collection, setCollection] = useState("");
+    const [tags, setTags] = useState<string[]>([]);
+
+    return (
+        <Card key={data.id}>
+            <CardHeader>
+                <CardTitle>{data.title}</CardTitle>
+                <CardDescription>{data.url}</CardDescription>
+                <CardContent>
+                    {data.description}
+                    <div>Collection: {data.collection.name}</div>
+                    <div className="flex gap-2">
+                        Tags:
+                        {data.tags.map((tag) => (
+                            <div key={tag.id}>{tag.name}</div>
+                        ))}
+                    </div>
+                </CardContent>
+            </CardHeader>
+            <div className="flex gap-3 flex-wrap">
+                <div className="flex items-center">
+                    <div className="mx-3">
+                        <LuPackage2 />
+                    </div>
+                    <CollectionSelector
+                        collections={collectionsQuery.data}
+                        value={collection}
+                        setValue={setCollection}
+                    />
+                </div>
+                <div className="flex items-center">
+                    <div className="mx-3">
+                        <LuTag />
+                    </div>
+                    <div className="flex gap-2 flex-wrap">
+                        {tags.map((tag, index) => (
+                            <TagSelector
+                                key={tag}
+                                index={index}
+                                tags={tagsQuery.data}
+                                value={tag}
+                                setValue={(newTag, index) =>
+                                    setTags(setTag(tags, newTag, index))
+                                }
+                                remove={(index) =>
+                                    setTags(removeTag(tags, index))
+                                }
+                            />
+                        ))}
+                        <TagSelector
+                            tags={tagsQuery.data}
+                            value=""
+                            index={-1}
+                            setValue={(newTag, index) =>
+                                setTags(setTag(tags, newTag, index))
+                            }
+                            remove={(index) => setTags(removeTag(tags, index))}
+                        />
+                    </div>
+                </div>
+            </div>
+        </Card>
+    );
+}
--- src/app/saves/page.tsx
+++ src/app/saves/page.tsx
@@ -1,16 +1,8 @@
 "use client";
 
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle,
-    LogInRequired,
-    PageTitle,
-    SaveInput,
-} from "../components";
+import { LogInRequired, PageTitle, SaveInput } from "../components";
 import { trpc } from "@/src/app/utils/trpc";
+import ItemCard from "../components/ItemCard";
 
 export default function SavesPage() {
     return (
@@ -32,24 +24,7 @@ function SaveList() {
     return (
         <div className="flex flex-col mt-3 gap-3">
             {items
-                ? items.map((item) => (
-                      <Card key={item.id}>
-                          <CardHeader>
-                              <CardTitle>{item.title}</CardTitle>
-                              <CardDescription>{item.url}</CardDescription>
-                              <CardContent>
-                                  {item.description}
-                                  <div>Collection: {item.collection.name}</div>
-                                  <div className="flex gap-2">
-                                      Tags:
-                                      {item.tags.map((tag) => (
-                                          <div key={tag.id}>{tag.name}</div>
-                                      ))}
-                                  </div>
-                              </CardContent>
-                          </CardHeader>
-                      </Card>
-                  ))
+                ? items.map((item) => <ItemCard data={item} key={item.id} />)
                 : null}
         </div>
     );
