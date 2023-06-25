fea996c685301bc3a91dcf07e826205a77cb3a1f
Max Xue <maxxue1512@gmail.com>
2023-06-26 02:45:59 +1000

    Fixed sign up style

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
@@ -9,6 +9,7 @@ import Link from "next/link";
 import { redirect } from "next/navigation";
 import { LuUser } from "react-icons/lu";
 import { Button, Input, PageTitle } from "../../../../ui/components";
+import { Eye, EyeOff } from "lucide-react";
 
 const userSchema = z.object({
     firstName: z.string().min(1, { message: "First name is required" }),
@@ -38,13 +39,6 @@ Add next auth
  */
 
 export default function page() {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm<SignUpFormData>({ resolver: zodResolver(userSchema) });
-    const createUserMutation = trpc.user.createUser.useMutation();
-
     const isLoggedInQuery = trpc.user.isLoggedIn.useQuery();
     useEffect(() => {
         if (isLoggedInQuery.data) {
@@ -52,40 +46,58 @@ export default function page() {
         }
     }, [isLoggedInQuery.data]);
 
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<SignUpFormData>({ resolver: zodResolver(userSchema) });
+    const createUserMutation = trpc.user.createUser.useMutation();
+
+    const [passwordShown, setPasswordShown] = useState<boolean>(false);
+
     return (
-        <div>
-            <div className="flex items-center">
-                <LuUser className="mr-5" size={36} />
-                <PageTitle>Sign Up</PageTitle>
-            </div>
+        <div className="flex items-center justify-center h-screen">
             <form
-                className="flex flex-col gap-3"
+                className="flex flex-col gap-3 w-[32rem] px-10"
                 action=""
                 onSubmit={handleSubmit((data: SignUpFormData) => {
                     createUserMutation.mutate(data);
                 })}
             >
-                <div>
-                    <Input
-                        type="text"
-                        title="First name"
-                        placeholder="First name"
-                        id="sign-up-first-name-input"
-                        {...register("firstName", { required: true })}
-                    />
-                    {errors.firstName && (
-                        <span>{errors.firstName.message}</span>
-                    )}
+                <div className="flex items-center self-start mb-4">
+                    <LuUser className="mr-3" size={36} strokeWidth={1.75} />
+                    {/* <PageTitle>Sign Up</PageTitle> */}
+                    <h1 className="text-3xl">Sign Up</h1>
                 </div>
-                <div>
-                    <Input
-                        type="text"
-                        title="Last name"
-                        placeholder="Last name"
-                        id="sign-up-last-name-input"
-                        {...register("lastName", { required: true })}
-                    />
-                    {errors.lastName && <span>{errors.lastName.message}</span>}
+                <div className="flex">
+                    <div className="mr-3">
+                        <Input
+                            type="text"
+                            title="First name"
+                            placeholder="First name"
+                            id="sign-up-first-name-input"
+                            {...register("firstName", { required: true })}
+                        />
+                        {errors.firstName && (
+                            <span className="mt-2 text-sm text-red-600/60">
+                                {errors.firstName.message}
+                            </span>
+                        )}
+                    </div>
+                    <div>
+                        <Input
+                            type="text"
+                            title="Last name"
+                            placeholder="Last name"
+                            id="sign-up-last-name-input"
+                            {...register("lastName", { required: true })}
+                        />
+                        {errors.lastName && (
+                            <span className="mt-2 text-sm text-red-600/60">
+                                {errors.lastName.message}
+                            </span>
+                        )}
+                    </div>
                 </div>
                 <div>
                     <Input
@@ -96,26 +108,45 @@ export default function page() {
                         {...register("email", { required: true })}
                     />
                     {errors.email ? (
-                        <span>{errors.email.message}</span>
+                        <span className="mt-2 text-sm text-red-600/60">
+                            {errors.email.message}
+                        </span>
                     ) : (
                         createUserMutation.error?.data?.code ===
                             "BAD_REQUEST" && (
-                            <span>{createUserMutation.error.message}</span>
+                            <span className="mt-2 text-sm text-red-600/60">
+                                {createUserMutation.error.message}
+                            </span>
                         )
                     )}
                 </div>
-                <div>
+                <div className="relative">
                     <Input
-                        type="password"
+                        type={passwordShown ? "text" : "password"}
                         title="Password"
                         placeholder="Password"
                         id="sign-up-password-input"
                         {...register("password", { required: true })}
                     />
-                    {errors.password && <span>{errors.password.message}</span>}
+                    <button
+                        onClick={() => setPasswordShown((prev) => !prev)}
+                        className="absolute w-fit right-[10px] top-[10px]"
+                        type="button"
+                    >
+                        {passwordShown ? (
+                            <EyeOff size={20} color="grey" />
+                        ) : (
+                            <Eye size={20} color="grey" />
+                        )}
+                    </button>
+                    {errors.password && (
+                        <span className="mt-2 text-sm text-red-600/60">
+                            {errors.password.message}
+                        </span>
+                    )}
                 </div>
                 <Button type="submit">Create account</Button>
-                <p>
+                <p className="text-sm">
                     Already have an account?{" "}
                     <Link className="underline" href={"/login"}>
                         Log in
