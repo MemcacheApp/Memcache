e7adc34364f2a57a5c8a22a2781ec48aa11dc5db
wirelessEye <okuloj@outlook.com>
2023-06-26 20:25:17 +1000

    remove ItemController.createItem

--- src/server/controllers/item-controller.ts
+++ src/server/controllers/item-controller.ts
@@ -5,44 +5,6 @@ import TagController from "./tag-controller";
 import ogs from "open-graph-scraper";
 
 export default class ItemController {
-    static async createItem(
-        userId: string,
-        type: string,
-        title: string,
-        url: string,
-        description: string,
-        thumbnail: string | null,
-        collectionName: string,
-        tagNames: string[],
-        siteName: string
-    ) {
-        const collection = await CollectionController.getOrCreateCollection(
-            userId,
-            collectionName
-        );
-        const tags = await TagController.getOrCreateTags(userId, tagNames);
-        const item = await prisma.item.create({
-            data: {
-                type,
-                id: uuidv4(),
-                status: 0,
-                collectionId: collection.id,
-                tags: {
-                    connect: tags.map((tag) => ({ id: tag.id })),
-                },
-                title,
-                url,
-                description,
-                thumbnail,
-                createdAt: new Date(),
-                userId,
-                siteName,
-            },
-        });
-
-        return item;
-    }
-
     static async createFromURL(
         userId: string,
         url: string,
@@ -51,17 +13,36 @@ export default class ItemController {
     ) {
         const { result } = await ogs({ url });
 
-        return await this.createItem(
+        const collection = await CollectionController.getOrCreateCollection(
             userId,
-            result.ogType || "website",
-            result.ogTitle || result.twitterTitle || "Untitled",
-            result.ogUrl || url,
-            result.ogDescription || result.dcDescription || "",
-            result.ogImage?.[0].url || result.twitterImage?.[0].url || null,
-            collectionName,
-            tagNames,
-            result.ogSiteName || new URL(url).hostname
+            collectionName
         );
+        const tags = await TagController.getOrCreateTags(userId, tagNames);
+
+        return await prisma.item.create({
+            data: {
+                type: result.ogType || "website",
+                id: uuidv4(),
+                status: 0,
+                collectionId: collection.id,
+                tags: {
+                    connect: tags.map((tag) => ({ id: tag.id })),
+                },
+                title: result.ogTitle || result.twitterTitle || "Untitled",
+                url: result.ogUrl || url,
+                description: result.ogDescription || result.dcDescription || "",
+                thumbnail:
+                    result.ogImage?.[0].url || result.twitterImage?.[0].url,
+                createdAt: new Date(),
+                userId,
+                siteName: result.ogSiteName || new URL(url).hostname,
+                duration: result.musicDuration
+                    ? parseInt(result.musicDuration)
+                    : undefined,
+                releaseTime: result.releaseDate,
+                author: result.author,
+            },
+        });
     }
 
     static async getItem(itemId: string) {
