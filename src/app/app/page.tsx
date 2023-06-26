dda8a48e3a4ef5516f79e6ac2035ccba03bb2814
wirelessEye <okuloj@outlook.com>
2023-06-26 17:16:09 +1000

    Fix routing

---
+++ src/app/app/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { redirect } from "next/navigation";
+import { trpc } from "@/src/app/utils/trpc";
+
+export default function page() {
+    const { data } = trpc.user.isLoggedIn.useQuery();
+
+    if (data === undefined) {
+        return <div>Loading...</div>;
+    } else if (data) {
+        // if logged in, redirect to saves
+        redirect("/app/saves");
+    } else {
+        // If not logged in, redirect to discover
+        redirect("/app/discover");
+    }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,5 @@
-"use client";
-
 import { redirect } from "next/navigation";
-import { trpc } from "@/src/app/utils/trpc";
 
 export default function page() {
-    const { data } = trpc.user.isLoggedIn.useQuery();
-
-    if (data === undefined) {
-        return <div>Loading...</div>;
-    } else if (data) {
-        // if logged in, redirect to saves
-        redirect("/app/saves");
-    } else {
-        // If not logged in, redirect to discover
-        redirect("/app/discover");
-    }
+    redirect("/app");
 }
