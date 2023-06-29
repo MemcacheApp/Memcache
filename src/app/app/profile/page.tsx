bbc6dd39196070330ed3dce993d8319e3c165a87
Max Xue <maxxue1512@gmail.com>
2023-06-29 22:00:23 +1000

    Added basic editing styling for profile page

--- src/app/app/profile/page.tsx
+++ src/app/app/profile/page.tsx
@@ -1,51 +1,102 @@
 "use client";
-import { Button } from "@/ui/components";
+import { Button, Input } from "@/ui/components";
 import { PageTitle } from "../../../../ui/components/typography";
 import { trpc } from "../../utils/trpc";
 import { Pencil } from "lucide-react";
 import { useState } from "react";
+import { ZodError, z } from "zod";
 
 function ProfileInfo({
     title,
     info,
-    edit,
+    submitEdit,
+    errorMessage,
 }: {
     title: string;
     info: string;
-    edit: (newInfo: string) => void;
+    submitEdit: (
+        newInfo: string,
+        setErrorMessage: (errMsg: string) => void
+    ) => void;
 }) {
     const [isEditing, setIsEditing] = useState<boolean>(false);
+    const [error, setError] = useState(null);
 
     return (
         <>
             <div className="">
                 <strong>{title}</strong>
             </div>
-            <div className="col-span-2">{info}</div>
-            <div className="justify-self-end">
-                <button
-                    type="button"
-                    className="p-2 rounded-md hover:bg-slate-200 w-fit h-fit"
-                    onClick={() => setIsEditing(true)}
+            <div className="col-span-2 ">
+                <form
+                    action=""
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        console.log("form submit");
+                    }}
                 >
-                    <Pencil size={20} />
-                </button>
+                    {isEditing ? (
+                        <Input
+                            placeholder={`Please enter your new ${title.toLowerCase()}`}
+                        />
+                    ) : (
+                        <span className="px-3 py-2 text-sm">{info}</span>
+                    )}
+                </form>
+            </div>
+            <div className="justify-self-end">
+                {isEditing ? (
+                    <div>
+                        <Button variant={"outline"} className="bg-white">
+                            Save
+                        </Button>
+                        <Button
+                            variant={"outline"}
+                            className="ml-1 bg-white"
+                            onClick={() => setIsEditing(false)}
+                        >
+                            Cancel
+                        </Button>
+                    </div>
+                ) : (
+                    <Button
+                        variant={"outline"}
+                        className="p-0 m-0 bg-white"
+                        onClick={() => setIsEditing(true)}
+                    >
+                        <Pencil size={20} />
+                    </Button>
+                )}
             </div>
         </>
     );
 }
+
+const emailSchema = z.string().min(1).email();
 
 export default function ProfilePage() {
     const updateEmailMutation = trpc.user.updateEmail.useMutation();
     return (
         <div className="flex flex-col">
             <PageTitle>Profile</PageTitle>
-            <div className="grid w-full max-w-6xl grid-cols-4 border-solid border-y-2 border-slate-200">
+            <div className="grid items-center w-full max-w-6xl grid-cols-4 gap-2 px-3 py-2 border-solid border-y-2 border-slate-200">
                 <ProfileInfo
                     title={"Email"}
                     info={"johnsmith@gmail.com"}
-                    edit={() => {
-                        console.log("Email");
+                    submitEdit={(
+                        newEmail: string,
+                        setErrorMessage: (errMsg: string) => void
+                    ) => {
+                        try {
+                            emailSchema.parse(newEmail);
+                            updateEmailMutation.mutate({ newEmail });
+                        } catch (e) {
+                            if (e instanceof ZodError) {
+                                setErrorMessage(e.message);
+                            } else {
+                                console.log(e);
+                            }
+                        }
                     }}
                 />
                 <ProfileInfo
