92821c0939ca182abc6f9fe510c16c399833a42b
Max Xue <maxxue1512@gmail.com>
2023-06-29 13:46:21 +1000

    Create profile route and page

---
+++ src/app/app/profile/page.tsx
@@ -0,0 +1,9 @@
+import { PageTitle } from "../../../../ui/components/typography";
+
+export default function ProfilePage() {
+    return (
+        <div className="flex flex-col">
+            <PageTitle>Profile</PageTitle>
+        </div>
+    );
+}
