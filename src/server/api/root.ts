36729370931a6b0f85b8c79090342ef1596aef7b
Gordon Huang <dqna64@gmail.com>
2023-07-08 04:05:19 +1000

    set up openai api for flashcard generator endpoint

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -3,12 +3,14 @@ import { itemRouter } from "./routes/item";
 import { userRouter } from "./routes/user";
 import { collectionRouter } from "./routes/collection";
 import { tagRouter } from "./routes/tag";
+import { flashcardsRouter } from "./routes/flashcards";
 
 export const appRouter = router({
     item: itemRouter,
     user: userRouter,
     collection: collectionRouter,
     tag: tagRouter,
+    flashcards: flashcardsRouter,
 });
 
 export type AppRouter = typeof appRouter;
--- src/server/controllers/errors/item.ts
+++ src/server/controllers/errors/item.ts
@@ -1,4 +1,4 @@
-import { createError } from "../../utils";
+import { createError } from "../../utils/error";
 
 export const CreateFromURLError = createError("CreateFromURLError", {
     FetchError: "Failed to fetch metadata",
