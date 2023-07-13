1fefffce129b57d95a2cc3e3823b4624e8cb6d2c
wirelessEye <okuloj@outlook.com>
2023-07-13 16:15:00 +1000

    SummariesPage: Add sections

--- src/app/app/summaries/page.tsx
+++ src/app/app/summaries/page.tsx
@@ -1,11 +1,22 @@
 "use client";
 
-import { PageTitle } from "../../../../ui/components";
+import { PageTitle } from "@/ui/components";
 
 export default function SummariesPage() {
     return (
         <div className="flex flex-col">
             <PageTitle>Summaries</PageTitle>
+            <div className="flex flex-col gap-5">
+                <div className="bg-background mx-8 p-6 border rounded-lg">
+                    <h2 className="text-xl font-semibold mb-5">My Summaries</h2>
+                    <h3 className="text-lg font-medium">Recently viewed</h3>
+                </div>
+                <div className="bg-background mx-8 p-6 border rounded-lg">
+                    <h2 className="text-xl font-semibold mb-5">
+                        Generate Summary
+                    </h2>
+                </div>
+            </div>
         </div>
     );
 }
