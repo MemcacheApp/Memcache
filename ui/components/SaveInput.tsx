12a17c3e0df5292bed10389f2ad7d528304aebca
wirelessEye <okuloj@outlook.com>
2023-07-14 21:12:57 +1000

    SaveInput: collapse options at the start

--- ui/components/SaveInput.tsx
+++ ui/components/SaveInput.tsx
@@ -62,30 +62,23 @@ function SaveInputPopover({ isShow, onDismiss }: SaveInputPopoverProps) {
     const [url, setUrl] = useState("");
     const [collection, setCollection] = useState("");
     const [tags, setTags] = useState<string[]>([]);
-    const [metadata, setMetadata] = useState<ItemMetadata | null>(null);
 
     const [isHidden, setIsHidden] = useState(true);
     const [isCollapse, setIsCollapse] = useState(true);
-    const [isLoading, setIsLoading] = useState(true);
 
     const inputRef = useRef<HTMLInputElement>(null);
-    const fetchMetadataTimer = useRef<number | NodeJS.Timeout | null>(null);
 
-    const collectionsQuery = trpc.collection.getUserCollections.useQuery();
-    const tagsQuery = trpc.tag.getUserTags.useQuery();
-    const fetchMetadataQuery = trpc.item.fetchMetadata.useQuery(
-        {
-            url,
-        },
-        { refetchOnWindowFocus: false, enabled: false }
-    );
     const createItemMutation = trpc.item.createItem.useMutation({
         onSuccess: () => ctx.item.getUserItems.invalidate(),
     });
 
-    const refresh = useCallback(() => {
-        fetchMetadataQuery.refetch();
-    }, []);
+    useEffect(() => {
+        if (createItemMutation.isSuccess) {
+            setUrl("");
+            setTags([]);
+            onDismiss();
+        }
+    }, [createItemMutation.isSuccess]);
 
     useEffect(() => {
         if (isShow) {
@@ -102,19 +95,138 @@ function SaveInputPopover({ isShow, onDismiss }: SaveInputPopoverProps) {
         }
     }, [isShow]);
 
+    const _onKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
+        if (e.key === "Escape") {
+            onDismiss();
+        }
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (url === "") return;
+        createItemMutation.mutate({
+            url,
+            collectionName: collection,
+            tagNames: tags,
+        });
+    };
+
+    return (
+        <div
+            className={cn("z-50", {
+                hidden: isHidden,
+            })}
+            onKeyDown={_onKeyDown}
+        >
+            <FocusScope trapped={isShow} loop>
+                <form
+                    className={cn(
+                        "@container flex flex-col absolute -left-1 -top-1 -right-1 z-10 transition-[transform,opacity]",
+                        isCollapse
+                            ? "opacity-0 scale-95"
+                            : "opacity-100 scale-100"
+                    )}
+                    action=""
+                    onSubmit={handleSubmit}
+                >
+                    <div
+                        className={cn(
+                            "relative h-12 bg-background shadow-lg rounded-t-lg border",
+                            url
+                                ? "@xl:rounded-br-md"
+                                : "rounded-br-md rounded-bl-md"
+                        )}
+                    >
+                        <Input
+                            className="absolute top-0 bottom-0 h-full bg-transparent text-base border-none px-4 z-10"
+                            placeholder="https://"
+                            value={url}
+                            onChange={(e) => setUrl(e.target.value)}
+                            ref={inputRef}
+                        />
+                    </div>
+                    <SaveOptions
+                        url={url}
+                        isCreating={createItemMutation.isLoading}
+                        collection={collection}
+                        setCollection={setCollection}
+                        tags={tags}
+                        setTags={setTags}
+                    />
+                </form>
+                <div
+                    className={cn(
+                        "fixed top-0 bottom-0 left-0 right-0 transition-[background-color,backdrop-filter]",
+                        {
+                            "bg-white/80 backdrop-blur-sm": !isCollapse,
+                        }
+                    )}
+                    onClick={onDismiss}
+                ></div>
+            </FocusScope>
+        </div>
+    );
+}
+
+interface SaveOptionsProps {
+    url: string;
+    isCreating: boolean;
+    collection: string;
+    setCollection: (collection: string) => void;
+    tags: string[];
+    setTags: (tags: string[]) => void;
+}
+
+function SaveOptions({
+    url,
+    isCreating,
+    collection,
+    setCollection,
+    tags,
+    setTags,
+}: SaveOptionsProps) {
+    const [isHidden, setIsHidden] = useState(true);
+    const [isCollapse, setIsCollapse] = useState(true);
+
+    const [metadata, setMetadata] = useState<ItemMetadata | null>(null);
+
+    const collectionsQuery = trpc.collection.getUserCollections.useQuery();
+    const tagsQuery = trpc.tag.getUserTags.useQuery();
+
+    const fetchMetadataTimer = useRef<number | NodeJS.Timeout | null>(null);
+    const fetchMetadataQuery = trpc.item.fetchMetadata.useQuery(
+        {
+            url,
+        },
+        { refetchOnWindowFocus: false, enabled: false }
+    );
+
+    const refresh = useCallback(() => {
+        fetchMetadataQuery.refetch();
+    }, []);
+
     useEffect(() => {
         if (url) {
+            setIsHidden(false);
+            setTimeout(() => {
+                setIsCollapse(false);
+            }, 10);
+
             if (fetchMetadataTimer.current) {
                 clearTimeout(fetchMetadataTimer.current);
                 fetchMetadataTimer.current = null;
             }
             fetchMetadataTimer.current = setTimeout(refresh, 2000);
+        } else {
+            setIsCollapse(true);
+            setTimeout(() => {
+                setIsHidden(true);
+            }, 200);
         }
     }, [url]);
 
     useEffect(() => {
         if (fetchMetadataQuery.isFetched) {
-            setIsLoading(false);
             if (fetchMetadataQuery.data) {
                 setMetadata(fetchMetadataQuery.data);
             } else {
@@ -124,24 +236,8 @@ function SaveInputPopover({ isShow, onDismiss }: SaveInputPopoverProps) {
                     siteName: hostname(url),
                 });
             }
-        } else {
-            setIsLoading(true);
         }
     }, [fetchMetadataQuery.isFetched]);
-
-    useEffect(() => {
-        if (createItemMutation.isSuccess) {
-            setUrl("");
-            setTags([]);
-            onDismiss();
-        }
-    }, [createItemMutation.isSuccess]);
-
-    const _onKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
-        if (e.key === "Escape") {
-            onDismiss();
-        }
-    };
 
     const setTag = (name: string, index: number) => {
         if (includeCaseInsensitive(tags, name)) {
@@ -167,128 +263,79 @@ function SaveInputPopover({ isShow, onDismiss }: SaveInputPopoverProps) {
         setTags(newTags);
     };
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        if (url === "") return;
-        createItemMutation.mutate({
-            url,
-            collectionName: collection,
-            tagNames: tags,
-        });
-    };
-
     return (
         <div
-            className={cn("z-50", {
-                hidden: isHidden,
-            })}
-            onKeyDown={_onKeyDown}
+            className={cn(
+                "relative bg-background p-3 max-w-xl rounded-b-lg border-b border-x shadow-lg transition-[transform,opacity]",
+                { hidden: isHidden, "-translate-y-5 opacity-0": isCollapse }
+            )}
         >
-            <FocusScope trapped={isShow} loop>
-                <form
-                    className={cn(
-                        "flex flex-col absolute -left-1 -top-1 -right-1 z-10 transition-[transform,opacity]",
-                        isCollapse
-                            ? "opacity-0 scale-95"
-                            : "opacity-100 scale-100"
-                    )}
-                    action=""
-                    onSubmit={handleSubmit}
-                >
-                    <div className="relative h-12 bg-background shadow-lg rounded-t-lg rounded-br-md border">
-                        <Input
-                            className="absolute top-0 bottom-0 h-full bg-transparent text-base border-none px-4 z-10"
-                            placeholder="https://"
-                            value={url}
-                            onChange={(e) => setUrl(e.target.value)}
-                            ref={inputRef}
+            <SimpleItemCard
+                loading={fetchMetadataQuery.isLoading}
+                url={url}
+                title={metadata?.title}
+                description={metadata?.description}
+                thumbnail={metadata?.thumbnail}
+                siteName={metadata?.siteName}
+                footerRight={
+                    fetchMetadataQuery.isLoading ? undefined : (
+                        <Button
+                            className="shrink-0"
+                            type="button"
+                            variant="icon"
+                            size="none"
+                            onClick={refresh}
+                            disabled={isCreating}
+                        >
+                            <RefreshCw size={18} />
+                        </Button>
+                    )
+                }
+            />
+            <div className="flex flex-col gap-3 my-4 mx-2">
+                <div className="flex gap-4 flex-wrap items-center">
+                    <Package2 className="text-slate-500" size={20} />
+                    <CollectionSelector
+                        collections={collectionsQuery.data}
+                        value={collection}
+                        setValue={setCollection}
+                        disabled={isCreating}
+                    />
+                </div>
+                <div className="flex gap-4 flex-wrap items-center">
+                    <Tag className="text-slate-500" size={20} />
+                    {tags.map((tag, index) => (
+                        <TagSelector
+                            key={tag}
+                            index={index}
+                            tags={tagsQuery.data}
+                            value={tag}
+                            setValue={setTag}
+                            remove={removeTag}
+                            disabled={isCreating}
                         />
-                    </div>
-                    <div className="relative bg-background p-3 max-w-xl rounded-b-lg border-b border-x shadow-lg">
-                        <SimpleItemCard
-                            loading={isLoading}
-                            url={url}
-                            title={metadata?.title}
-                            description={metadata?.description}
-                            thumbnail={metadata?.thumbnail}
-                            siteName={metadata?.siteName}
-                            footerRight={
-                                isLoading ? undefined : (
-                                    <Button
-                                        className="shrink-0"
-                                        type="button"
-                                        variant="icon"
-                                        size="none"
-                                        onClick={refresh}
-                                        disabled={createItemMutation.isLoading}
-                                    >
-                                        <RefreshCw size={18} />
-                                    </Button>
-                                )
-                            }
-                        />
-                        <div className="flex flex-col gap-3 my-4 mx-2">
-                            <div className="flex gap-4 flex-wrap items-center">
-                                <Package2
-                                    className="text-slate-500"
-                                    size={20}
-                                />
-                                <CollectionSelector
-                                    collections={collectionsQuery.data}
-                                    value={collection}
-                                    setValue={setCollection}
-                                    disabled={createItemMutation.isLoading}
-                                />
-                            </div>
-                            <div className="flex gap-4 flex-wrap items-center">
-                                <Tag className="text-slate-500" size={20} />
-                                {tags.map((tag, index) => (
-                                    <TagSelector
-                                        key={tag}
-                                        index={index}
-                                        tags={tagsQuery.data}
-                                        value={tag}
-                                        setValue={setTag}
-                                        remove={removeTag}
-                                        disabled={createItemMutation.isLoading}
-                                    />
-                                ))}
-                                <TagSelector
-                                    tags={tagsQuery.data}
-                                    value=""
-                                    index={-1}
-                                    setValue={setTag}
-                                    remove={removeTag}
-                                    disabled={createItemMutation.isLoading}
-                                />
-                            </div>
-                        </div>
-                        <div className="flex items-center gap-3 justify-end">
-                            <Loader
-                                varient="ring"
-                                className={cn("w-6 h-6", {
-                                    hidden: !createItemMutation.isLoading,
-                                })}
-                            />
-                            <Button
-                                type="submit"
-                                disabled={createItemMutation.isLoading}
-                            >
-                                Save
-                            </Button>
-                        </div>
-                    </div>
-                </form>
-                <div
-                    className={cn(
-                        "fixed top-0 bottom-0 left-0 right-0 transition-[background-color,backdrop-filter]",
-                        {
-                            "bg-white/80 backdrop-blur-sm": !isCollapse,
-                        }
-                    )}
-                    onClick={onDismiss}
-                ></div>
-            </FocusScope>
+                    ))}
+                    <TagSelector
+                        tags={tagsQuery.data}
+                        value=""
+                        index={-1}
+                        setValue={setTag}
+                        remove={removeTag}
+                        disabled={isCreating}
+                    />
+                </div>
+            </div>
+            <div className="flex items-center gap-3 justify-end">
+                <Loader
+                    varient="ring"
+                    className={cn("w-6 h-6", {
+                        hidden: !isCreating,
+                    })}
+                />
+                <Button type="submit" disabled={isCreating}>
+                    Save
+                </Button>
+            </div>
         </div>
     );
 }
