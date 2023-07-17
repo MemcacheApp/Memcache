26e296a76e5ec6a7d3cd80dba6c70722355a6c34
Gordon Huang <dqna64@gmail.com>
2023-07-17 17:27:52 +1000

    add sections on flashcards page

--- src/app/app/flashcards/page.tsx
+++ src/app/app/flashcards/page.tsx
@@ -1,7 +1,15 @@
 "use client";
 
+import { Button, Card, Input } from "@/ui/components";
+import ExternalLink from "@/ui/components/ExternalLink";
+import { H4 } from "@/ui/components/typography";
+import { Collection, Item, Tag } from "@prisma/client";
+import { Globe, Package2 } from "lucide-react";
+import Link from "next/link";
+import { useState } from "react";
 import { H3, PageTitle } from "../../../../ui/components/typography";
 import { ReviewRatingEnum } from "../../utils/ReviewRating";
+import { trpc } from "../../utils/trpc";
 
 const flashcardsData = [
     {
@@ -54,8 +62,20 @@ const flashcardsData = [
 ];
 
 export default function FlashcardsPage() {
+    const [url, setUrl] = useState("");
+
+    const itemsQuery = trpc.item.getUserItems.useQuery();
+
+    const suggestedItems =
+        itemsQuery.data
+            // sort in reverse chronological order of creation date
+            // Could sort by more complex combinations of factors to suggest items
+            // to generate flashcards for e.g. articles rather than social media posts
+            ?.sort((a, b) => b.createdAt.valueOf() - a.createdAt.valueOf())
+            .slice(0, 8) ?? [];
+
     return (
-        <div className="flex flex-col">
+        <div className="flex flex-col mx">
             <PageTitle>Flashcards</PageTitle>
             <p>{`Prompt: "Recently I've picked up a new hobby: I'm learning to fly light aircraft and I find it very fun and interesting. However I have a lot to learn."`}</p>
             <p>{`(please don't click this too much lol it cost money)`}</p>
@@ -74,9 +94,97 @@ export default function FlashcardsPage() {
                     </div>
                 </div>
             ))}
-            <div>
-                <H3>Review</H3>
-            </div>
+            <Card className="rounded-lg px-6 py-6">
+                <H3>Generate Flashards</H3>
+                <div className="mt-3 mb-4">
+                    <Input
+                        className="text-base border-solid rounded-md"
+                        placeholder="https://"
+                        value={url}
+                        onChange={(e) => setUrl(e.target.value)}
+                    />
+                </div>
+                <H4>Suggested</H4>
+                <div className="mt-3 mb-4 w-full pb-4 flex flex-row flex-nowrap gap-4 overflow-x-scroll">
+                    {suggestedItems.map((item) => (
+                        <SuggestedItemCard key={item.id} item={item} />
+                    ))}
+                </div>
+            </Card>
+            <Card className="rounded-lg px-6 py-6">
+                <H3>My Flashcards</H3>
+                <H4>Revision Queue</H4>
+                <H4>Recently Viewed</H4>
+            </Card>
+            <Card className="rounded-lg px-6 py-6">
+                <H3>Items with Flashcards</H3>
+                <H4>Recently Created</H4>
+            </Card>
         </div>
+    );
+}
+
+function SuggestedItemCard({
+    item,
+}: {
+    item: Item & { collection: Collection; tags: Tag[] };
+}) {
+    return (
+        <Card className="w-[380px] h-[460px] shrink-0 rounded-lg px-6 py-6">
+            {item.title}
+            {item.description}
+            {item.siteName}
+            <div className="flex flex-wrap gap-5 text-slate-450 text-sm">
+                {item.siteName ? (
+                    <ExternalLink
+                        href={item.url ? item.url : "#"}
+                        onClick={(e) => {
+                            e.stopPropagation();
+                        }}
+                    >
+                        <div className="h-full flex items-center gap-2">
+                            {item.favicon ? (
+                                <img
+                                    width={16}
+                                    height={16}
+                                    src={item.favicon}
+                                />
+                            ) : (
+                                <Globe size={16} />
+                            )}
+
+                            {item.siteName}
+                        </div>
+                    </ExternalLink>
+                ) : null}
+                {item.collection ? (
+                    <Link href={`/app/collection/${item.collection.id}`}>
+                        <div className="h-full flex items-center gap-2">
+                            <Package2 size={16} />
+                            {item.collection.name}
+                        </div>
+                    </Link>
+                ) : null}
+                {item.tags ? (
+                    <div className="flex flex-wrap gap-3">
+                        {item.tags.map((tag) => (
+                            <Link
+                                key={tag.id}
+                                href={`/app/tag/${tag.id}`}
+                                tabIndex={-1}
+                            >
+                                <Button
+                                    className="px-4"
+                                    variant="secondary"
+                                    size="xs"
+                                >
+                                    {tag.name}
+                                </Button>
+                            </Link>
+                        ))}
+                    </div>
+                ) : null}
+            </div>
+        </Card>
     );
 }
