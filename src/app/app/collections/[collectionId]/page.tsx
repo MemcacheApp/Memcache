790b6a5d856028122bb9d51faf48ce586718b449
William Zhang <williamzhang1478@gmail.com>
2023-07-18 02:24:10 +1000

    Implemented collection page

--- src/app/app/collections/[collectionId]/page.tsx
+++ src/app/app/collections/[collectionId]/page.tsx
@@ -1,5 +1,17 @@
 "use client";
 
+import {
+    LogInRequired,
+    PageTitle,
+    SaveInput,
+    ItemPanel,
+    WithPanel,
+    ItemList,
+} from "@/ui/components";
+import { useItemListStore } from "@/src/app/store/item-list";
+import { useEffect } from "react";
+import { trpc } from "@/src/app/utils/trpc";
+
 interface CollectionPageProps {
     params: {
         collectionId: string;
@@ -7,7 +19,31 @@ interface CollectionPageProps {
 }
 
 export default function CollectionPage({ params }: CollectionPageProps) {
-    const collectionId = params.collectionId;
+    const resetStates = useItemListStore((state) => state.reset);
 
-    return <div>Collection {collectionId}&apos;s content goes here</div>;
+    useEffect(() => {
+        resetStates();
+    }, []);
+
+    const collectionQuery = trpc.collection.getCollection.useQuery({
+        collectionId: params.collectionId,
+    });
+    const collection = collectionQuery.data;
+
+    if (!collection) {
+        return <PageTitle>This collection doesn&apos;t exist.</PageTitle>;
+    }
+
+    return (
+        <div className="flex flex-col">
+            <LogInRequired>
+                <WithPanel>
+                    <PageTitle>Collection: {collection.name}</PageTitle>
+                    <SaveInput />
+                    <ItemList collectionId={params.collectionId} />
+                </WithPanel>
+                <ItemPanel />
+            </LogInRequired>
+        </div>
+    );
 }
