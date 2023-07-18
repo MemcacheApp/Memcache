d838cc964644892155b6a1e3f7bd1fa126072023
wirelessEye <okuloj@outlook.com>
2023-07-18 20:02:51 +1000

    Add `AllSummariesPage`

---
+++ src/app/app/summaries/all/page.tsx
@@ -0,0 +1,9 @@
+import { PageTitle } from "@/ui/components";
+
+export default function AllSummariesPage() {
+    return (
+        <div>
+            <PageTitle>All Summaries</PageTitle>
+        </div>
+    );
+}
