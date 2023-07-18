364b3e2c573d3fd73417a86b666250644f7857f5
Max Xue <maxxue1512@gmail.com>
2023-07-18 23:56:51 +1000

    Adding flashcard to db and adding getFlashcards

--- src/server/controllers/flashcard-controller.ts
+++ src/server/controllers/flashcard-controller.ts
@@ -1,5 +1,6 @@
 import { Experience, Range } from "@/src/datatypes/flashcard";
 import ContentScraper from "@/src/utils/content-scraper";
+import { Flashcard } from "@prisma/client";
 import {
     ChatCompletionFunctions,
     ChatCompletionRequestMessage,
@@ -61,6 +62,18 @@ const functions: ChatCompletionFunctions[] = [
 ];
 
 export default class FlashcardController {
+    static async getUserFlashcards(userId: string) {
+        // TODO: error check
+        const flashcards = await prisma.flashcard.findMany({
+            where: {
+                userId,
+            },
+            include: {
+                reviews: true,
+            },
+        });
+        return flashcards;
+    }
     static async generateFlashcards(
         userId: string,
         itemId: string,
@@ -73,7 +86,7 @@ export default class FlashcardController {
             url: item.url,
         });
         const truncatedContent = content.slice(0, 5000); // About 1000 words
-        console.log(`Content: ${truncatedContent}`);
+        // console.log(`Content: ${truncatedContent}`);
 
         const systemPrompt = generateSystemPrompt(
             item.description,
@@ -100,7 +113,6 @@ export default class FlashcardController {
 
         for (let i = 0; i < numOfFlashcards; i++) {
             // TODO: Lengths over 13000 chars but under 18000 chars work, > 18000 chars don't
-
             const chatCompletion = await openai.createChatCompletion({
                 model: "gpt-3.5-turbo", // $0.002USD per 1K tokens (don't use GPT-4 it's 30x more expensive)
                 // n: 4, // Number of flashcards to generate
@@ -113,12 +125,12 @@ export default class FlashcardController {
                     name: functionName, // Force the AI to call this function on next chat response
                 },
             });
-
             const response_message = chatCompletion.data.choices[0].message;
+            console.log(response_message);
 
-            console.log(
-                `\nGenerating flashcard ${i} (AI makes function call):`
-            );
+            // console.log(
+            //     `\nGenerating flashcard ${i} (AI makes function call):`
+            // );
             // console.dir(chatCompletion.data, { depth: null });
 
             // Check if the AI wants to call a function
@@ -137,19 +149,30 @@ export default class FlashcardController {
                         const functionCallResult = getFlashcard(
                             parsedGetFlashcardParams
                         );
-                        console.dir(functionCallResult, { depth: null });
+
+                        console.log(functionCallResult);
+                        // console.dir(functionCallResult, { depth: null });
                         const today = new Date();
                         const tomorrow = new Date(today);
                         tomorrow.setDate(tomorrow.getDate() + 1);
                         tomorrow.setHours(0, 0, 0, 0);
-                        await prisma.flashcard.create({
-                            data: {
-                                ...functionCallResult,
-                                itemId,
-                                userId,
-                                dueDate: tomorrow, // Initial due date when flashcard is created is the next day
-                            },
-                        });
+                        // TODO: error handling here
+                        console.log("first");
+                        const newFlashcard: Flashcard = {
+                            id: functionCallResult.id,
+                            question: functionCallResult.question,
+                            answer: functionCallResult.answer,
+                            itemId,
+                            userId,
+                            dueDate: tomorrow, // Initial due date when flashcard is created is the next day
+                        };
+                        console.log(newFlashcard);
+                        try {
+                            prisma.flashcard.create({ data: newFlashcard });
+                        } catch (error) {
+                            console.log(error);
+                        }
+                        console.log("second");
 
                         // Put function call response into message history for the AI to read
                         messages.push(response_message); // This is a ChatCompletionResponseMessage authored by the AI assistant
