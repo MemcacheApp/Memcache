c4867b934defd1e192e1d7e9955515e9a4973d0a
wirelessEye <okuloj@outlook.com>
2023-07-27 21:18:56 +1000

    DiscoverPage: Masonry grid

--- src/app/app/discover/page.tsx
+++ src/app/app/discover/page.tsx
@@ -1,24 +1,48 @@
 "use client";
 
+import { SuggestedItem } from "@/src/datatypes/item";
 import { Loader, SimpleItemCard } from "@/ui/components";
 import { PageTitle } from "@/ui/components/typography";
+import { useMemo } from "react";
 import { trpc } from "../../utils/trpc";
 
 export default function DiscoverPage() {
     const getSuggestedItemsQuery = trpc.discovery.getSuggestedItems.useQuery();
 
+    const columns = useMemo(() => {
+        if (getSuggestedItemsQuery.data) {
+            const numColumn = 3;
+            const result: SuggestedItem[][] = Array.from(
+                { length: numColumn },
+                () => [],
+            );
+
+            for (let i = 0; i < getSuggestedItemsQuery.data.length; i++) {
+                const index = i % numColumn;
+                result[index].push(getSuggestedItemsQuery.data[i]);
+            }
+            return result;
+        } else {
+            return undefined;
+        }
+    }, [getSuggestedItemsQuery.data]);
+
     return (
         <div className="@container">
             <PageTitle>Discover</PageTitle>
             <div className="md:mx-8 mb-8 grid grid-cols-1 @xl:grid-cols-2 @3xl:grid-cols-3 gap-3">
-                {getSuggestedItemsQuery.data ? (
-                    getSuggestedItemsQuery.data.map((item) => (
-                        <SimpleItemCard
-                            key={item.url}
-                            thumbnail={item.thumbnail}
-                            title={item.title}
-                            description={item.description}
-                        />
+                {columns !== undefined ? (
+                    columns.map((column, i) => (
+                        <div className="grid gap-3" key={i}>
+                            {column.map((item) => (
+                                <SimpleItemCard
+                                    key={item.url}
+                                    thumbnail={item.thumbnail}
+                                    title={item.title}
+                                    description={item.description}
+                                />
+                            ))}
+                        </div>
                     ))
                 ) : (
                     <Loader varient="ellipsis" />
