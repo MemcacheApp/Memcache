f37433b00ea12db7386500fa71bf38534306893e
wirelessEye <okuloj@outlook.com>
2023-07-27 20:46:15 +1000

    DiscoveryPage: basic frontend

--- src/app/app/discover/page.tsx
+++ src/app/app/discover/page.tsx
@@ -1,11 +1,25 @@
 "use client";
 
-import { PageTitle } from "../../../../ui/components/typography";
+import { ItemCard, Loader } from "@/ui/components";
+import { PageTitle } from "@/ui/components/typography";
+import { trpc } from "../../utils/trpc";
 
 export default function DiscoverPage() {
+    const getSuggestedItemsQuery =
+        trpc.discovery.getSuggestedDiscoveryItems.useQuery();
+
     return (
-        <div className="flex flex-col">
+        <div className="@container">
             <PageTitle>Discover</PageTitle>
+            <div className="md:mx-8 mb-8 grid grid-cols-1 @xl:grid-cols-2 @3xl:grid-cols-3 gap-3">
+                {getSuggestedItemsQuery.data ? (
+                    getSuggestedItemsQuery.data.map((item) => (
+                        <ItemCard key={item.id} data={item} />
+                    ))
+                ) : (
+                    <Loader varient="ellipsis" />
+                )}
+            </div>
         </div>
     );
 }
