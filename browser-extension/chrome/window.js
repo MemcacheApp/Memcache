062b040d316cc0c52827bba15be02c88039ee2cd
wirelessEye <okuloj@outlook.com>
2023-08-01 19:56:10 +1000

    Fix browser extension

--- browser-extension/chrome/window.js
+++ browser-extension/chrome/window.js
@@ -2,5 +2,5 @@ const page = document.getElementById("page");
 
 chrome.tabs.query({ active: true, lastFocusedWindow: true }).then((result) => {
     console.log(result[0].url);
-    page.setAttribute("src", `http://localhost:3000/browser?url=${result[0].url}`);
+    page.setAttribute("src", `http://localhost:3000/browser-extension?url=${result[0].url}`);
 });
